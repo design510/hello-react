8fa44b66c6d2cfa68aa7f945f97e7981230899cf
xiansheng.qu <xiansheng.qu@gmail.com>
2022-05-23 23:49:30 +0800

    更新配置文件

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import "./App.css";
+import "./App.less";
 import React, { Component } from "react";
+import { Button } from 'antd';
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
 import Container from "./components/TodoList/Container";
@@ -8,6 +9,12 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <br />
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
         <Hello nickName="大家好呀！"></Hello>
         <Container />
         <Welcome name="React" />
